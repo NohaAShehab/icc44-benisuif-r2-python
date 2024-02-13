50c18a5e5ce423daed793229076c8dc14acfb44d
Noha Shehab <63148220+NohaAShehab@users.noreply.github.com>
2024-02-13 10:15:23 +0200

    sets

---
+++ sets.py
@@ -0,0 +1,20 @@
+
+myset = {"iti","ahmed", "iti", 3234, "python", 435.34, True, None }
+
+print(myset)
+"""
+    remove duplication 
+    un-ordered datatype 
+    no index 
+"""
+""" mutable data type ? """
+myset.add("abc")
+print(myset)
+
+"remove element"
+# popped_element = myset.pop()
+# print(popped_element)
+# print(myset)
+
+myset.remove("iti")
+print(myset)
